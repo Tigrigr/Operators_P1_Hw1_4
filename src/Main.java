fdd05bdc6c8242ba049edbb29a1b327c637d8d04
mikha <Mikhailov.dzenphone@gmail.com>
2023-03-27 12:19:54 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -11,4 +12,15 @@ public class Main {
             System.out.println("Если возраст человека равен " + age + ", он не достиг совершеннолетия, нужно немного подождать.");
         }
     }
+    public static void task2 () {
+        System.out.println();
+        System.out.println("Задача 2");
+        int temperature = 4;
+        if (temperature<5){
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
+        }
+    }
+
 }
